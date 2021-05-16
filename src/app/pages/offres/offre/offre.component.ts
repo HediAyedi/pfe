8011d7cf6d3905b9ee3d1f2ae62691aa66aa70b2
Hedi Ayedi <hediayedi20@gmail.com>
2021-05-16 01:59:32 +0100

    consuming data for offre detail page and adding 50% of work

--- src/app/pages/offres/offre/offre.component.ts
+++ src/app/pages/offres/offre/offre.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Offre} from '../../../models/offre';
+import {OffreService} from '../../../api/offres.service';
 
 @Component({
   selector: 'app-offre',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OffreComponent implements OnInit {
 
-  constructor() { }
+  idRoute: any;
+  offre: Offre = new Offre() ;
+  offres: Offre[] = [] ;
+  constructor(private route: ActivatedRoute,
+              private offreService: OffreService) { }
 
   ngOnInit(): void {
+    this.idRoute = this.route.snapshot.paramMap.get('offre_id');
+    console.log('activated route : ' + this.idRoute);
+    // this.offres = JSON.parse(localStorage.getItem("employeursCache"));
+    // console.log("Offres : " + this.offres.id);
+    this.findAll();
+
+    // this.offre = this.offres.filter(
+    //     off => off.id === this.idRoute
+    //   ) ;
+
+
+  }
+
+  public findAll() {
+    this.offreService.getAll()
+      .subscribe(data => {
+        this.offres = data;
+        console.log(this.offres) ;
+
+        // this.offre = this.offres.filter(
+        //   off => off.id === this.idRoute
+        // ) ;
+
+        // console.log(this.offres);
+
+
+        // console.log("data :::: :: :: :" , data);
+        // console.log("offffffffffres:::: :: :: :" , JSON.parse(String(this.offre.id)));
+
+        // console.log(this.offres);
+      }, err => {
+        console.log(err);
+      });
   }
 
 }
